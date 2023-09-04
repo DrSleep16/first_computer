a3ce6dc0053f923587a44cbdd90b71a426ee3f7d
Dmitry <Dr.Sleep1995@gmail.com>
2023-09-04 21:56:17 +0300

    get_pdf_text

--- main.py
+++ main.py
@@ -14,16 +14,18 @@ def get_audio_from_text(text, language):
     audio.save(file_name)
 
 
+def get_pdf_text(pdf):
+    with pdfplumber.PDF(pdf) as pdf_file:
+        pages_pdf_text = [page.extract_text() for page in pdf_file.pages]
+        pdf_text = ' '.join(pages_pdf_text)
+        return pdf_text
+
+
 def main():
     file_path = "text.pdf"
     with open(file_path, mode='rb') as file:
         pdf = BytesIO(file.read())
-
-    with pdfplumber.PDF(pdf) as pdf_file:
-        pages_pdf_text = [page.extract_text() for page in pdf_file.pages]
-        pdf_text = ' '.join(pages_pdf_text)
-        print(pdf_text)
-
+    pdf_text = get_pdf_text(pdf)
     get_audio_from_text(pdf_text, 'ru')
 
 
